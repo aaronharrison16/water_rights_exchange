2324c746e42187bd63423c894c8a9882c7acdddc
Aaron Harrison <aaronharrison16@gmail.com>
2018-09-14 12:56:24 -0600

    added the ability to create posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -13,3 +13,24 @@ describe 'navigate' do
     end
   end
 end
+
+describe 'creation' do
+  before do 
+    visit new_post_path
+  end
+  
+  it 'has a new form that can be reached' do 
+    expect(page.status_code).to eq(200)
+  end
+
+  it 'can be created from the new form page' do
+    fill_in 'post[title]', with: 'Test Title'
+    fill_in 'post[available]', with: '88'
+    fill_in 'post[price]', with: '1800'
+    fill_in 'post[address]', with: "123 West Any Street"
+    fill_in 'post[location]', with: 'Any Location'
+    click_on 'Save'
+
+    expect(page).to have_content("Test Title")
+  end
+end
