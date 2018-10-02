e4dba724ec32dbc425d9d669667de68a488cc058
Aaron Harrison <aaronharrison16@gmail.com>
2018-10-02 13:33:37 -0600

    Blocked users from editing posts that have been approved

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,24 @@
 class PostPolicy < ApplicationPolicy
   def update?
-    record.user_id == user.id || user.type == 'AdminUser'
+    return true if post_approved? && admin?
+    return true if user_or_admin && !post_approved?
   end
+
+  def approve?
+    admin?
+  end  
+
+  private
+
+    def user_or_admin
+      record.user_id == user.id || admin?
+    end
+
+    def admin?
+      user.type == "AdminUser"
+    end
+
+    def post_approved?
+      record.approved?
+    end
 end
