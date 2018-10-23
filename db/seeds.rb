d285084fc16c2f49ce5c92a89035b595b6ae08a2
Aaron Harrison <aaronharrison16@gmail.com>
2018-10-23 11:15:27 -0600

    modified seeds file to populate production values

--- db/seeds.rb
+++ db/seeds.rb
@@ -84,28 +84,31 @@ divisions_list = [
 [ 8, 'Rush Valley'],
 [ 8, 'Davis County']]
 
-divisions_list.each do |region_id, division|
-  Division.create!(region_id: region_id, name: division)
+case Rails.env
+when 'development'
+  divisions_list.each do |region_id, division|
+    Division.create!(region_id: region_id, name: division)
+  end
+
+  puts 'divisions created'
+
+  AdminUser.create!(email: "admin@user.com", password: 'password', password_confirmation: 'password', first_name: 'Admin', last_name: 'User', roles: 'site_admin', phone: '4564564561')
+
+  puts 'admin user created'
+
+  @user_2 = User.create!(email: "aaronharrison@waterrightexchange.com", password: 'password', password_confirmation: 'password', first_name: 'Aaron', last_name: 'Harrison', phone: '3333333333')
+
+  @user = User.create!(email: "test@user.com", password: 'password', password_confirmation: 'password', first_name: 'Ron', last_name: 'Swanson', phone: '5555555555')
+
+  puts '2 test users created'
+
+  50.times do |post|
+    Post.create!(title: "Test Title #{post}", available: 12, price: 1500, address: "123 West Any Street", location: "Any Location", user_id: @user.id, purpose: 'buy', water_type: 'acre_foot' )
+  end
+
+  50.times do |post|
+    Post.create!(title: "Test Title User 2 #{post}", available: 12, price: 1500, address: "123 West Any Street", location: "Any Location", user_id: @user_2.id, purpose: 'buy', water_type: 'share' )
+  end
+
+  puts '100 posts created'
 end
-
-puts 'divisions created'
-
-AdminUser.create!(email: "admin@user.com", password: 'password', password_confirmation: 'password', first_name: 'Admin', last_name: 'User', roles: 'site_admin', phone: '4564564561')
-
-puts 'admin user created'
-
-@user_2 = User.create!(email: "aaronharrison@waterrightexchange.com", password: 'password', password_confirmation: 'password', first_name: 'Aaron', last_name: 'Harrison', phone: '3333333333')
-
-@user = User.create!(email: "test@user.com", password: 'password', password_confirmation: 'password', first_name: 'Ron', last_name: 'Swanson', phone: '5555555555')
-
-puts '2 test users created'
-
-50.times do |post|
-  Post.create!(title: "Test Title #{post}", available: 12, price: 1500, address: "123 West Any Street", location: "Any Location", user_id: @user.id, purpose: 'buy', water_type: 'acre_foot' )
-end
-
-50.times do |post|
-  Post.create!(title: "Test Title User 2 #{post}", available: 12, price: 1500, address: "123 West Any Street", location: "Any Location", user_id: @user_2.id, purpose: 'buy', water_type: 'share' )
-end
-
-puts '100 posts created'
