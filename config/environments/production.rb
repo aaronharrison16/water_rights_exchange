36d97fbf654083f2d9135ed30099a63d3330869f
Aaron Harrison <aaronharrison16@gmail.com>
2018-12-06 00:45:33 -0700

    added missing line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => 'utah-water-right-exchange.herokuapp.com' }
+
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   ActionMailer::Base.smtp_settings = {
@@ -100,7 +102,6 @@ Rails.application.configure do
    :authentication       => :plain,
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
-   :openssl_verify_mode  => 'none',
-   :only_path            => true
+   :openssl_verify_mode  => 'none'
   }
 end
