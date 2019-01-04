e72a6c79b92edebee2a211e8bd71c92d42e8e73b
Aaron Harrison <aaronharrison16@gmail.com>
2019-01-04 16:53:02 -0700

    fixed the bug where a user cannot me created

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
   has_many :posts
   
-  validates_presence_of :first_name, :last_name, :phone
+  validates_presence_of :first_name, :last_name
     
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
